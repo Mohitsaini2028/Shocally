ecd225aa789301ef15a78efae8476f2f589b32eb
Mohit Saini <mohitsaini00720@gmail.com>
2021-09-15 17:30:18 +0530

    update

--- shocally/settings.py
+++ shocally/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -119,5 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS=[
+    STATIC_DIR,
+]
+
 
 AUTH_USER_MODEL='shop.User'
--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('pinInput', views.pincodeInput, name='pincodeInput'),
+    # path('', views.home, name='home'),
+    path('', views.pincodeInput, name='pincodeInput'),
     path('pinResult', views.pinResult, name='pinResult'),
 ]
