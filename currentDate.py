2e4c0a07e28ed67b1d0238895850a076ae992df7
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2021-11-11 22:05:46 -0800

    Update currentDate.py

--- currentDate.py
+++ currentDate.py
@@ -1,21 +1,45 @@
 from datetime import date
 todaydate = date.today()
 todate = str(todaydate)
-filetype = ".txt"
-filename = todate + filetype
-ip='127.0.0.1'
-with open(filename, "a+") as f:
-    
 
-    f.seek(0)
-    #read content of file to string
-    data = f.readlines()
-    print(data)
-    #get number of occurrences of the substring in the string
-    occurrences = data.count(f"{ip}\n")
+def get_filename(word):
+    filetype = ".txt"
+    filename = word+'_'+todate + filetype
+    return filename
 
-    if occurrences<5:
+def update_ip(word,ID,ip): 
+    filename = get_filename(word)   
+    with open(filename, "a+") as f:
+        f.seek(0)
+        #read content of file to string
+        data = f.readlines()
+        #print(data)
+        #get number of occurrences of the substring in the string
+        occurrences = data.count(f"{ID},{ip}\n")
+
+        if occurrences<5:         
+            f.writelines(f"{ID},{ip}\n")
+            print("Inserted in Database")
+        print('Number of occurrences of the word :', occurrences) 
         
-        f.writelines(f"{ip}\n")
-        print("Inserted in Database")
-    print('Number of occurrences of the word :', occurrences)
+
+#                  filename, id, ip  
+def check_occurrences(word,ID,ip): 
+    filename = get_filename(word)
+    with open(filename, "r") as f:
+        f.seek(0)
+        #read content of file to string
+        data = f.readlines()
+        #print(data)
+        #get number of occurrences of the substring in the string
+        occurrences = data.count(f"{ID},{ip}\n")
+    return occurrences
+
+def main():
+    word = 'shop' 
+    ip='127.0.0.1'
+    ID = 4 #shopid #newsid
+    update_ip(word,ID,ip)
+    print(check_occurrences(word,ID,ip))
+
+
