dd026cd5a9abf93fa13c677b2b5e5612e0ab5256
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2021-11-11 22:40:38 -0800

    Update currentDate.py

--- currentDate.py
+++ currentDate.py
@@ -7,9 +7,9 @@ def get_filename(word):
     filename = word+'_'+todate + filetype
     return filename
 
-def update_ip(word,ID,ip): 
-    filename = get_filename(word)   
-    with open(filename, "a+") as f:
+def update_ip(word,ID,ip):    
+    filename = get_filename(word) 
+    with open(filename, "a+") as f:   
         f.seek(0)
         #read content of file to string
         data = f.readlines()
@@ -17,29 +17,35 @@ def update_ip(word,ID,ip):
         #get number of occurrences of the substring in the string
         occurrences = data.count(f"{ID},{ip}\n")
 
-        if occurrences<5:         
+        if occurrences<5:
+            
             f.writelines(f"{ID},{ip}\n")
             print("Inserted in Database")
-        print('Number of occurrences of the word :', occurrences) 
+        print('Number of occurrences of the word :', occurrences)
         
 
-#                  filename, id, ip  
-def check_occurrences(word,ID,ip): 
+#                 filename, id, ip  
+def check_occurrences(word,ID): 
     filename = get_filename(word)
     with open(filename, "r") as f:
         f.seek(0)
         #read content of file to string
         data = f.readlines()
-        #print(data)
+
+        ids = []
+        for i in data:
+            i = i.split(',')
+            i = i[0]
+            ids.append(i)
+              
         #get number of occurrences of the substring in the string
-        occurrences = data.count(f"{ID},{ip}\n")
+        occurrences = ids.count(f"{ID}")
+        print(f"OCCURRENCES of {ID} is :",occurrences)
     return occurrences
 
 def main():
     word = 'shop' 
-    ip='127.0.0.1'
+    ip='445456.565.456456.455,127.0.0.1'
     ID = 4 #shopid #newsid
-    update_ip(word,ID,ip)
-    print(check_occurrences(word,ID,ip))
-
-
+    #update_ip(word,ID,ip)
+    print(check_occurrences(word,ID))
