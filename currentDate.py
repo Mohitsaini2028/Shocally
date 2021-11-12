1b6ac01c3b10d9772315b59696807b02e7074ec6
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2021-11-11 23:00:21 -0800

    Update currentDate.py

--- currentDate.py
+++ currentDate.py
@@ -25,27 +25,37 @@ def update_ip(word,ID,ip):
         
 
 #                 filename, id, ip  
-def check_occurrences(word,ID): 
+def check_occurrences(word): 
     filename = get_filename(word)
+    id_views = {}
     with open(filename, "r") as f:
         f.seek(0)
         #read content of file to string
         data = f.readlines()
 
         ids = []
+        unique = set()
         for i in data:
             i = i.split(',')
             i = i[0]
             ids.append(i)
-              
-        #get number of occurrences of the substring in the string
-        occurrences = ids.count(f"{ID}")
-        print(f"OCCURRENCES of {ID} is :",occurrences)
-    return occurrences
+            unique.add(i)
+        print("ids",ids)
+        print("unique",unique)
+
+        
+
+        for i in unique:
+            #get number of occurrences of the substring in the string
+            occurrences = ids.count(f"{i}")
+            id_views[i]= occurrences
+            print(f"OCCURRENCES of {i} is :",occurrences)
+    return id_views  #returning a dictionary where key is id and value is views
 
 def main():
     word = 'shop' 
     ip='445456.565.456456.455,127.0.0.1'
     ID = 4 #shopid #newsid
     #update_ip(word,ID,ip)
-    print(check_occurrences(word,ID))
+    print(check_occurrences(word))
+
